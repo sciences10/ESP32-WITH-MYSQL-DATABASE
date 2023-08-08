1e994f6883ac7843bd81370598bbc4de988920c1
wen633 <aiot63324@gmail.com>
2023-08-08 14:35:37 +0800

    front

--- ESP32_MySQL_Database/Final/recordtable.php
+++ ESP32_MySQL_Database/Final/recordtable.php
@@ -3,6 +3,8 @@
   <head>
     <title>智慧車用感測</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
     <style>
       html {font-family: Arial; display: inline-block; text-align: center;}
@@ -97,6 +99,15 @@
         pointer-events:none;
       }
       /* ----------------------------------- */
+      h3 {
+        display: block;
+        font-size: 1.17em;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        font-weight: bold;
+      }
 
       #table_id th{
         background-color: #0c6980;
