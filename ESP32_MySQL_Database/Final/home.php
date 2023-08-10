f77011e193074db36c98b6c115ecab4add7b69a6
wen633 <aiot63324@gmail.com>
2023-08-10 13:25:11 +0800

    front

--- ESP32_MySQL_Database/Final/home.php
+++ ESP32_MySQL_Database/Final/home.php
@@ -241,6 +241,23 @@
     </div>
 
     <br>
+
+    <div class="content">
+        <div class="cards">
+            <div class="card">
+                <div class="card header">
+                    <h3 style="font-size: 1rem;">CO2</h3>
+                </div>
+                
+                <div style="padding: 10px;">
+                    <iframe class="iframeSet" max-width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2226167/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=CO2&type=column&yaxis=e"></iframe>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <br>
     
     <div class="content">
         <div class="cards">
@@ -250,8 +267,7 @@
                 </div>
                 
                 <div style="padding: 10px;">
-                    <iframe class="iframeSet" max-width="450" height="260" style="border: 1px solid #cccccc;"
-                    src="https://thingspeak.com/channels/2226167/charts/1?width=auto&bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=%E8%BB%8A%E8%B7%9D&type=spline&yaxis=%C2%B0C"></iframe>
+                    <iframe class="iframeSet" max-width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/2226167/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=%E8%BB%8A%E8%B7%9D&type=spline&yaxis=CM"></iframe>
                 </div>
 
             </div>
